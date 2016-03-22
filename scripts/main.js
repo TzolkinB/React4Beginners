efe13d2f41a5d1ca5bc2a206a3be06c2351cdde1
Kimberly Bell <kimberly.bell@tma1.com>
2016-03-22 16:20:38 -0400

    multiple components

--- scripts/main.js
+++ scripts/main.js
@@ -3,20 +3,65 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // to use ReactDOM, needed for browser
 
-// StorePicker
+// App
+class App extends React.Component{
+	render() {
+		return(
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header />
+				</div>
+				<Order />
+				<Inventory />
+			</div>
+		)
+	}
+};
 
+// Header
+class Header extends React.Component{
+	render() {
+		return(
+			<p>Header</p>
+		)
+	}
+}
+
+// Order
+class Order extends React.Component{
+	render() {
+		return(
+			<p>Order</p>
+		)
+	}
+}
+
+
+// Inventory
+class Inventory extends React.Component{
+	render() {
+		return(
+			<p>Inventory</p>
+		)
+	}
+}
+// StorePicker
 class StorePicker extends React.Component{
 //var StorePicker = React.createClass
 	render() {
 	// render: function()	
+		var name = 'Me';
 		return(
-			<div>
-				<h1>Hello World</h1>
-			</div>
+			<form className="store-selector" action="">
+			{/* using Emmet form + ctrl e */}
+				<h1>Type Something</h1>
+				<input type="text" ref="storeId"/>
+				<input type="Submit"/>
+			</form>
 		);
 	}
 };
 
 // ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
 var mountPoint = document.querySelector('#main');
-ReactDOM.render(<StorePicker/>, mountPoint);
+ReactDOM.render(<App/>, mountPoint);
